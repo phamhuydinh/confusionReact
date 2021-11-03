05183d7af17a4bd6cbe302747c0c44f28b98d808
Pham Dinh Huy <huypham110399@gmail.com>
2021-11-03 08:06:28 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -10,9 +21,10 @@ function App() {
           <NavbarBrand href="/">Dinh Huy</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
-}
+  }
+};
 
 export default App;
